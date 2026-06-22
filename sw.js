e9d2f43c4920dc74735ae83b3975293bab218a0a
mosbahm623-glitch <mosbahm623@gmail.com>
2026-06-22 11:16:38 +0300

    chore: bump cache to lft-v131 (revert net cash formula to correct version)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Legacy Core — Service Worker
 // Network First للملفات الرئيسية عشان التحديثات تظهر فوراً
 
-const CACHE = 'lft-v130';
+const CACHE = 'lft-v131';
 const SHELL = [
   './',
   './index.html',
