15751b94cc5165e209294c605acd59b91f3631dd
mosbahm623-glitch <mosbahm623@gmail.com>
2026-06-22 12:02:49 +0300

    chore: bump cache to lft-v132 (seq range report)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Legacy Core — Service Worker
 // Network First للملفات الرئيسية عشان التحديثات تظهر فوراً
 
-const CACHE = 'lft-v131';
+const CACHE = 'lft-v132';
 const SHELL = [
   './',
   './index.html',
