fe9061fd2d0ecb22742026f5d5bf6eb63426de58
mosbahm623-glitch <mosbahm623@gmail.com>
2026-06-25 01:53:14 +0300

    chore: bump cache to lft-v193

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Legacy Core — Service Worker
 // Network First للملفات الرئيسية عشان التحديثات تظهر فوراً
 
-const CACHE = 'lft-v192';
+const CACHE = 'lft-v193';
 const SHELL = [
   './',
   './index.html',
